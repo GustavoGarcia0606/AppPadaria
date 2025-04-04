e63709bf687beb52549e1ac54ebb99464baf9950
GustavoGarcia <gu.garciamarinho@gmail.com>
2025-04-03 21:51:14 -0300

    update

--- src/view/Cliente.java
+++ src/view/Cliente.java
@@ -1,5 +1,32 @@
 package view;
 
-public class Cliente {
-    public static void main(String[] args) {}
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+import view.components.Sidebar;
+
+public class Cliente extends Application {
+
+    public void start(Stage janela) throws Exception {
+        Sidebar menu = new Sidebar();
+        Image imgIconStarbucks = new Image(getClass().getResourceAsStream("/view/resources/img/starbucks-48px.png"));
+        BorderPane layoutPrincipal = new BorderPane();
+        layoutPrincipal.setLeft(menu);
+        menu.setStyle("-fx-background-color: #FAFAFA; -fx-padding: 50px;-fx-image: url('/view/resources/img/starsbucks-48px.png')");
+
+        layoutPrincipal.setStyle("-fx-background-image: url('/view/resources/img/background.jpg')");
+
+
+        Scene scene = new Scene(layoutPrincipal,1024, 700);
+        janela.setScene(scene);
+        janela.setTitle("Starbucks");
+        janela.getIcons().add(imgIconStarbucks);
+        janela.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
--- src/view/components/Sidebar.java
+++ src/view/components/Sidebar.java
@@ -1,84 +1,103 @@
 package view.components;
 
+import javafx.geometry.Insets;
 import javafx.scene.control.Button;
-
 import javafx.scene.control.Label;
-
 import javafx.scene.image.Image;
-
 import javafx.scene.image.ImageView;
-
-import javafx.scene.layout.HBox;
-
 import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
 
 public class Sidebar extends VBox {
 
     public Button btnFornecedor = new Button("Fornecedor");
-
     public Button btnProduto = new Button("Produto");
-
     public Button btnCliente = new Button("Cliente");
-
     public Button btnPedido = new Button("Pedido");
 
+
     //construtor para inicializar o componentes (como um objeto) em outras janelas
-
     public Sidebar() {
+        Image imgIconFornecedor = new Image(getClass().getResourceAsStream("/view/resources/img/icon-fornecedor.png"));
+        Image  imgIconProduto = new Image(getClass().getResourceAsStream("/view/resources/img/icon-produto.png"));
+        Image  imgIconCliente = new Image(getClass().getResourceAsStream("/view/resources/img/icons8-cliente-40.png"));
+        Image  imgIconPedido = new Image(getClass().getResourceAsStream("/view/resources/img/icon-pedido.png"));
+        Image imgIconStarbucks = new Image(getClass().getResourceAsStream("/view/resources/img/starbucks96px.png"));
 
-        ImageView iconCliente = new ImageView( new Image(getClass().getResourceAsStream("/view/img/icon-cliente.png")));
+        //carregamento de fonts
+        Font fonteNegrito = Font.loadFont(getClass().getResourceAsStream("/view/resources/fonts/Jost-ExtraBold.ttf"), 14);
+        Font fonteLigth = Font.loadFont(getClass().getResourceAsStream("/view/resources/fonts/Jost-Light.ttf"), 10);
 
-        ImageView iconProduto = new ImageView(new Image(getClass().getResourceAsStream("/view/img/icon-produto.png")));
+        //determina como as imagens serao vizualizada
+        ImageView imgViewIconStarbucks = new ImageView(imgIconStarbucks);
+        imgViewIconStarbucks.setFitWidth(50);
+        imgViewIconStarbucks.setFitHeight(50);
 
-        ImageView IconPedido = new ImageView(new Image(getClass().getResourceAsStream("/view/img/icon-pedido.png")));
 
-        ImageView IconFornecedor = new ImageView(new Image(getClass().getResourceAsStream("/view/img/icon-fornecedor.png")));
+        ImageView imgViewIconFornecedor = new ImageView(imgIconFornecedor);
+        imgViewIconFornecedor.setFitHeight(20);//altura
+        imgViewIconFornecedor.setFitWidth(20);//largura
+        btnFornecedor.setGraphic(imgViewIconFornecedor);
 
-        iconCliente.setFitHeight(25);
+        ImageView imgViewProduto = new ImageView(imgIconProduto);
+        imgViewProduto.setFitHeight(20);
+        imgViewProduto.setFitWidth(20);
+        btnProduto.setGraphic(imgViewProduto);
 
-        iconCliente.setFitWidth(25);
+        ImageView imgViewCliente = new ImageView(imgIconCliente);
+        imgViewCliente.setFitHeight(20);
+        imgViewCliente.setFitWidth(20);
+        btnCliente.setGraphic(imgViewCliente);
 
-        iconProduto.setFitHeight(25);
-
-        iconProduto.setFitWidth(25);
-
-        IconPedido.setFitHeight(25);
-
-        IconPedido.setFitWidth(25);
-
-        IconFornecedor.setFitHeight(25);
-
-        IconFornecedor.setFitWidth(25);
-
-        btnPedido.setGraphic((IconPedido));
+        ImageView imgViewPedido = new ImageView(imgIconPedido);
+        imgViewPedido.setFitHeight(20);
+        imgViewPedido.setFitWidth(20);
+        btnPedido.setGraphic(imgViewPedido);
 
         String styleButton = "-fx-background-color: transparent;" +
-                             "-fx-border-color: transparent;" +
-                              "-fx-font-size: 14px;" +
-                              "fx-font-weight: bold;" +
-                              "fx-text-fill: green;" +
-                               "fx-alignment: center;" +
-                                "fx-graphic-text-gap: 10px" ;
-        btnCliente.setStyle(styleButton);
-
-        btnProduto.setStyle(styleButton);
-
+                "-fx-border-color: transparent;"+
+                "-fx-font-size: 18px;"+
+                "-fx-text-fill: black;" +
+                "-fx-alignment: center;"+
+                "-fx-graphic-text-gap: 20px;"+
+                "-fx-font-family:" + fonteLigth.getFamily() +";"+
+                     "-fx-cursor: hand;";
         btnFornecedor.setStyle(styleButton);
-
+        btnProduto.setStyle(styleButton);
+        btnCliente.setStyle(styleButton);
         btnPedido.setStyle(styleButton);
 
-        btnFornecedor.setGraphic((IconFornecedor));
-
-        btnCliente.setGraphic((iconCliente));
-
-        btnProduto.setGraphic((iconProduto));
-
         Label lblMenu = new Label("Starbucks");
+        lblMenu.setFont(Font.font(fonteNegrito.getFamily(),28));
+    lblMenu.setGraphic(imgViewIconStarbucks);
+    lblMenu.setGraphicTextGap(20);
 
-        lblMenu.setStyle("-fx-font-size: 16px; -fx-font-weight: bold;");
+     btnCliente.setOnMouseEntered(evento -> btnCliente.setStyle(styleButton.replace(" transparent", "#58b558")));
+
+     btnCliente.setOnMouseExited(evento ->btnCliente.setStyle(styleButton.replace(" #58b558", "transparent")));
+
+
+        btnPedido.setOnMouseEntered(evento -> btnPedido.setStyle(styleButton.replace(" transparent", "#58b558")));
+
+        btnPedido.setOnMouseExited(evento ->btnPedido.setStyle(styleButton.replace(" #58b558", "transparent")));
+
+
+        btnProduto.setOnMouseEntered(evento -> btnProduto.setStyle(styleButton.replace(" transparent", "#58b558")));
+
+        btnProduto.setOnMouseExited(evento ->btnProduto.setStyle(styleButton.replace(" #58b558", "transparent")));
+
+
+        btnFornecedor.setOnMouseEntered(evento -> btnFornecedor.setStyle(styleButton.replace(" transparent", "#58b558")));
+
+        btnFornecedor.setOnMouseExited(evento ->btnFornecedor.setStyle(styleButton.replace(" #58b558", "transparent")));
+
+
+
+
+        setMargin(lblMenu,new Insets(0,0,20,0));
+        //superior ,direita, inferior, esquerda
 
         getChildren().addAll(lblMenu, btnFornecedor, btnProduto, btnCliente, btnPedido);
 
     }
-
 }
